80ef7c1f793898646c8857a1d954e5a0c768a75d
ltbinh812 <tienbinh21999@gmail.com>
2025-10-23 23:14:35 +0700

    add
    Signed-off-by: ltbinh812 <tienbinh21999@gmail.com>

---
+++ src/player.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Player{
+    public:
+        int Score;
+};
+
+
+void Init_Player();
---
+++ src/scoring.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "scoring.h"
+#include "player.h"
+#include "board.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void calc_place_score(){
+    int size = board.size();
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            if(board[i][j] == 0){
+                
+            }
+        }
+    }
+}
+
+void Run(){
+    calc_place_score();
+
+}
+
