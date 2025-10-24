1da9383c9670a621adde156b70fb2631a4862edb
nttien255 <tiennt.ti.2225@gmail.com>
2025-10-24 13:33:19 +0700

    test
    test

---
+++ build/CMakeFiles/GoGame.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/university/CS102/GoGame/src/board.cpp" "CMakeFiles/GoGame.dir/src/board.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/board.cpp.obj.d"
+  "D:/university/CS102/GoGame/src/main.cpp" "CMakeFiles/GoGame.dir/src/main.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/main.cpp.obj.d"
+  "D:/university/CS102/GoGame/src/move.cpp" "CMakeFiles/GoGame.dir/src/move.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/move.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GoGame.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoGame.dir/src/board.cpp.obj"
+  "CMakeFiles/GoGame.dir/src/board.cpp.obj.d"
+  "CMakeFiles/GoGame.dir/src/main.cpp.obj"
+  "CMakeFiles/GoGame.dir/src/main.cpp.obj.d"
+  "CMakeFiles/GoGame.dir/src/move.cpp.obj"
+  "CMakeFiles/GoGame.dir/src/move.cpp.obj.d"
+  "GoGame.exe"
+  "GoGame.exe.manifest"
+  "GoGame.pdb"
+  "libGoGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
